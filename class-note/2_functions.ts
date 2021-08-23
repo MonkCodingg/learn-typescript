025a4bc7f01d4517a1e27e8a977ae331dbeba728
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-23 16:45:50 +0900

    함수 타입-옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -15,4 +15,11 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 
-sum(10, 20, 30, 40);
+sum(10);
+
+// 함수 옵셔널 파라미터
+function log(a: string, b?: string) {
+
+}
+log('hello world');
+log('hello ts', 'abc');
