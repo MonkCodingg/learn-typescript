445a581e7b3085c26aeed933dd3b177ea0930864
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-23 15:30:56 +0900

    기본 타입 - 문자열, 숫자, 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// js 문자열 선언
+//var str = 'hello';
+
+// ts 문자열 선언
+let str: string = 'hello';
+
+// ts 숫자
+let num: number = 10;
+
+// ts 배열
+let arr: Array<number> = [1, 2, 3];
+let heross: Array<string> = ['Capt', 'Thor', 'Hulk', 10];
+let items: number[] = [1, 2, 3];
