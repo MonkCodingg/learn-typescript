786f801f094ef26ed8418383849d1b4e1789e672
Seunghwan Jeon <laevus.j@gmail.com>
2021-09-03 11:42:16 +0900

    유니온 타입 - 연산자를 이용한 타입 정의 2

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -4,8 +4,32 @@
 // logMessage('hello');
 // logMessage(100);
 
+var seho: string | number | boolean;
 function logMessage(value: string | number) {
-  console.log(value);
+  if (typeof value === 'number') {
+    value.toLocaleString();
+  }
+  if (typeof value === 'string') {
+    value.toString();
+  }
+  throw new TypeError('value must be string of number');
 }
 logMessage('hello');
 logMessage(100);
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function asksomeone(someone: Developer | Person) {
+  someone.name;
+  someone.skill;
+  someone.age;
+}
+
