5e74125155530deb5962a6cc68c66a595ca75af1
SeungHwan Jeon <43638373+MonkCodingg@users.noreply.github.com>
2021-09-09 11:22:03 +0900

    기존 문법과 제네릭 차이 - 유니온 타입 이용한 선언 방식 문제

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -15,18 +15,26 @@
 // logText<string>('하이');
 
 // 기존 타입
-function logText(text: string) {
+// function logText(text: string) {
+//   console.log(text);
+//   text.split('').reverse().join('');
+//   return text;
+// }
+
+// 기존 타입 - 함수 중복 선언
+// function LogNumber(num: number) {
+//   console.log(num);
+//   return num;
+// }
+
+//유니온 타입을 이용한 선언 방식 문제점
+function logText(text: string | number) {
   console.log(text);
-  text.split('').reverse().join('');
   return text;
 }
-// 기존 타입 - 함수 중복 선언
-function LogNumber(num: number) {
-  console.log(num);
-  return num;
-}
 
-logText('a');
+const a = logText('a');
+a.split
 logText(10);
 // const num = LogNumber(10);
 // logText(true);
