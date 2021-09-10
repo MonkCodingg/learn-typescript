cb061c96d4c2680e30551d84e15838f2a1c3a49a
SeungHwan Jeon <43638373+MonkCodingg@users.noreply.github.com>
2021-09-10 15:19:53 +0900

    제네릭 - 정의된 타입으로 타입 제한

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -63,3 +63,27 @@ interface Dropdown<T> {
   selected: boolean;
 }
 const obj: Dropdown<string> = { value: 'abc', selected: false };
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach(function (text) {
+//     console.log(text);
+//   });
+//   return text;
+// }
+
+//logTextLength<string>(['hi', 'abc']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+logTextLength('a');
+logTextLength(10);
+logTextLength({ leng: 10 });
+
