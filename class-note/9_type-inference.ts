b328b496f0daaf1b90468069104c78832699e7b5
MonkCodingg <laevus.j@gmail.com>
2021-09-15 16:25:20 +0900

    인터페이스와 제네릭을 이용한 타입 추론 방식

---
+++ class-note/9_type-inference.ts
@@ -0,0 +1,19 @@
+var a = 10;
+
+function getB(b = 10) {
+    var c = 'hi';
+    return b + c;
+}
+
+10 + '10' // 1010
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+    value: T;
+    title: string;
+}
+
+var shoppingItem: Dropdown<string> = {
+    value: 'abc',
+    title: 'hello'
+}
